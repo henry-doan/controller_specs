7ab6dcc549f0ba491c2ac3dcee9ccbf2c62c53c8
Henry Doan <henrydoan96@gmail.com>
2018-06-05 19:58:20 -0600

    start test

--- spec/controllers/bank_accounts_controller_spec.rb
+++ spec/controllers/bank_accounts_controller_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe BankAccountsController, type: :controller do
 
+  # login users before all the test
+  login_user
+
+  # an attribute we can use to test to pass
+  let(:valid_attributes) {
+    { institution: 'Chase', amount: 200, active: true }
+  }
+
+   # an attribute we can use to test to fail
+  let(:invalid_attributes) {
+    { institution: '', amount: 200, active: true }
+  }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
