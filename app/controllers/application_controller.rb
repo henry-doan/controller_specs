532917f9450e868cdb578d2d6a869d380a89b4dd
Henry Doan <henrydoan96@gmail.com>
2018-06-05 19:48:07 -0600

    ready for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    # forces user login for all actions on all controllers
+    before_action :authenticate_user!
 end
